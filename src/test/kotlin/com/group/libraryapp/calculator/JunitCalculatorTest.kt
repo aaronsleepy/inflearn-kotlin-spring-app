58ffb4c0df76990328d6f15cc196a642ccc46b40
aaronsleepy <aaron@kmong.com>
2022-08-09 16:15:49 +0900

    test: Add remaining calculator test by junit5

--- src/test/kotlin/com/group/libraryapp/calculator/JunitCalculatorTest.kt
+++ src/test/kotlin/com/group/libraryapp/calculator/JunitCalculatorTest.kt
@@ -2,6 +2,8 @@ package com.group.libraryapp.calculator
 
 import org.assertj.core.api.AssertionsForClassTypes.assertThat
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
+import java.lang.IllegalArgumentException
 
 class JunitCalculatorTest {
     @Test
@@ -15,4 +17,53 @@ class JunitCalculatorTest {
         // then
         assertThat(calculator.number).isEqualTo(8)
     }
-}  
+
+    @Test
+    fun minusTest() {
+        // given
+        val calculator = Calculator(5)
+
+        // when
+        calculator.minus(3)
+
+        // then
+        assertThat(calculator.number).isEqualTo(2)
+    }
+
+    @Test
+    fun multiplyTest() {
+        // given
+        val calculator = Calculator(5)
+
+        // when
+        calculator.multiply(3)
+
+        // then
+        assertThat(calculator.number).isEqualTo(15)
+    }
+
+    @Test
+    fun divideTest() {
+        // given
+        val calculator = Calculator(5)
+
+        // when
+        calculator.divide(2)
+
+        // then
+        assertThat(calculator.number).isEqualTo(2)
+    }
+
+    @Test
+    fun divideByZeroThrowsExceptionTest() {
+        // given
+        val calculator = Calculator(5)
+
+        // when & then
+        val message = assertThrows<IllegalArgumentException> {
+            calculator.divide(0)
+        }.message
+
+        assertThat(message).isEqualTo("Can't divide by zero")
+    }
+}
