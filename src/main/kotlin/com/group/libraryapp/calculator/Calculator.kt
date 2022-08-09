24d3724d6f7eb24d017cbd250bf03847d4a38233
aaronsleepy <aaron@kmong.com>
2022-08-09 15:32:10 +0900

    test: Testby backing property

--- src/main/kotlin/com/group/libraryapp/calculator/Calculator.kt
+++ src/main/kotlin/com/group/libraryapp/calculator/Calculator.kt
@@ -1,24 +1,27 @@
 package com.group.libraryapp.calculator
 
 class Calculator(
-    var number: Int,
+    private var _number: Int,
 ) {
+    val number: Int
+        get() { this._number }
+
     fun add(operand: Int) {
-        this.number += operand
+        this._number += operand
     }
 
     fun minus(operand: Int) {
-        this.number -= operand
+        this._number -= operand
     }
 
     fun multiply(operand: Int) {
-        this.number *= operand
+        this._number *= operand
     }
 
     fun divide(operand: Int) {
         if (operand == 0) {
             throw IllegalArgumentException("Can't divide by zero")
         }
-        this.number /= operand
+        this._number /= operand
     }
 }
