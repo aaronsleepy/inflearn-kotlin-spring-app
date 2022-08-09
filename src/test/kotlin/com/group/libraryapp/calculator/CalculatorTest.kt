cae553720e54f84fdc041308c93b31450093685a
aaronsleepy <aaron@kmong.com>
2022-08-09 15:50:56 +0900

    test: Add divide testcase

--- src/test/kotlin/com/group/libraryapp/calculator/CalculatorTest.kt
+++ src/test/kotlin/com/group/libraryapp/calculator/CalculatorTest.kt
@@ -1,5 +1,6 @@
 package com.group.libraryapp.calculator
 
+import java.lang.Exception
 import kotlin.IllegalArgumentException
 
 fun main() {
@@ -7,6 +8,8 @@ fun main() {
     calculatorTest.addTest()
     calculatorTest.minusTest()
     calculatorTest.multiplyTest()
+    calculatorTest.divideTest()
+    calculatorTest.divideByZeroTest()
 }
 
 class CalculatorTest {
@@ -48,5 +51,39 @@ class CalculatorTest {
         if (calculator.number != 15) {
             throw IllegalArgumentException("addTest failed")
         }
+    }
+
+    fun divideTest() {
+        // given
+        val calculator = Calculator(5)
+
+        // when
+        calculator.divide(2)
+
+        // then
+        if (calculator.number != 2) {
+            throw IllegalArgumentException("addTest failed")
+        }
+    }
+
+    fun divideByZeroTest() {
+        // given
+        val calculator = Calculator(5)
+
+        // when
+        try {
+            calculator.divide(0)
+        } catch (e: java.lang.IllegalArgumentException) {
+            // then
+            if (e.message != "Can't divide by zero") {
+                throw IllegalArgumentException("divideByZeroTest failed")
+            }
+            // success
+            return
+        } catch (e: Exception) {
+            throw IllegalArgumentException("divideByZeroTest failed")
+        }
+
+        throw IllegalArgumentException("divideByZeroTest failed")
     }
 }
