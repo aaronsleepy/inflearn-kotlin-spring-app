473f3d393b2cb3385f5f5c6c4ad27402145311fc
aaronsleepy <aaron@kmong.com>
2022-08-09 16:09:02 +0900

    test: Add junit5 addTest

---
+++ src/test/kotlin/com/group/libraryapp/calculator/JunitCalculatorTest.kt
@@ -0,0 +1,18 @@
+package com.group.libraryapp.calculator
+
+import org.assertj.core.api.AssertionsForClassTypes.assertThat
+import org.junit.jupiter.api.Test
+
+class JunitCalculatorTest {
+    @Test
+    fun addTest() {
+        // given
+        val calculator = Calculator(5)
+
+        // when
+        calculator.add(3)
+
+        // then
+        assertThat(calculator.number).isEqualTo(8)
+    }
+}  
