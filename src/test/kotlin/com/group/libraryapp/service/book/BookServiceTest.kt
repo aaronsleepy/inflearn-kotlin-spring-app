862e594425292728aa5f2815bfe69f0f86f9cc6b
aaronsleepy <aaron@kmong.com>
2022-08-09 17:13:36 +0900

    test: Add saveBook testcase

---
+++ src/test/kotlin/com/group/libraryapp/service/book/BookServiceTest.kt
@@ -0,0 +1,31 @@
+package com.group.libraryapp.service.book
+
+import com.group.libraryapp.domain.book.BookRepository
+import com.group.libraryapp.dto.book.request.BookRequest
+import org.assertj.core.api.AssertionsForInterfaceTypes.assertThat
+import org.junit.jupiter.api.AfterEach
+import org.junit.jupiter.api.DisplayName
+import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class BookServiceTest @Autowired constructor(
+    private val bookService: BookService,
+    private val bookRepository: BookRepository,
+) {
+    @Test
+    @DisplayName("책 등록이 정상 동작한다")
+    fun saveBook() {
+        // given
+        val request = BookRequest("이상한 나라의 앨리스")
+
+        // when
+        bookService.saveBook(request)
+
+        // then
+        val books = bookRepository.findAll()
+        assertThat(books).hasSize(1)
+        assertThat(books[0].name).isEqualTo("이상한 나라의 앨리스")
+    }
+}
