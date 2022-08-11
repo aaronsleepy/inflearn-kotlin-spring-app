cfd0d88e9a079f40b721af08fef40943c0dc18f2
aaronsleepy <aaron@kmong.com>
2022-08-11 14:29:35 +0900

    refactoring: Add BookType enum

--- src/main/kotlin/com/group/libraryapp/domain/book/Book.kt
+++ src/main/kotlin/com/group/libraryapp/domain/book/Book.kt
@@ -9,7 +9,7 @@ import javax.persistence.Id
 class Book(
     val name: String,
 
-    val type: String,
+    val type: BookType,
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -24,7 +24,7 @@ class Book(
     companion object {
         fun fixture(
             name: String = "책 이름",
-            type: String = "COMPUTER",
+            type: BookType = BookType.COMPUTER,
             id: Long? = null,
         ): Book {
             return Book(
