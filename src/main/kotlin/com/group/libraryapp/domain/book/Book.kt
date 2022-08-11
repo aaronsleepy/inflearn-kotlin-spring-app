9d9012cd4b66962f0e98b3270b7ca229be030754
aaronsleepy <aaron@kmong.com>
2022-08-11 14:33:32 +0900

    fix: Add Enumerated to BookType enum

--- src/main/kotlin/com/group/libraryapp/domain/book/Book.kt
+++ src/main/kotlin/com/group/libraryapp/domain/book/Book.kt
@@ -1,14 +1,12 @@
 package com.group.libraryapp.domain.book
 
-import javax.persistence.Entity
-import javax.persistence.GeneratedValue
-import javax.persistence.GenerationType
-import javax.persistence.Id
+import javax.persistence.*
 
 @Entity
 class Book(
     val name: String,
 
+    @Enumerated(EnumType.STRING)
     val type: BookType,
 
     @Id
